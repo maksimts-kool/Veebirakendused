adcaa72baff2852a35720babe07b170da288078a
maksimts-kool <maksimtsitkool@gmail.com>
2025-10-30 12:18:50 +0200

    Update veeb11

--- html_tood/veeb11/script.js
+++ html_tood/veeb11/script.js
@@ -1,50 +1,110 @@
 let opilased = [
-  { eesnimi: "Igor", perekonnanimi: "Aleksejev", sugu: "Mees", link: "https://igoraleksejev24.thkit.ee" },
-  { eesnimi: "Illia", perekonnanimi: "Blahun", sugu: "Mees", link: "https://illiablahun24.thkit.ee" },
-  { eesnimi: "Nikita", perekonnanimi: "Gontsarov", sugu: "Mees", link: "https://nikitagontsarov24.thkit.ee" },
-  { eesnimi: "Mark", perekonnanimi: "Jürgen", sugu: "Mees", link: "https://markjurgen24.thkit.ee" },
-  { eesnimi: "Nikita", perekonnanimi: "Litvinenko", sugu: "Mees", link: "https://nikitalitvinenko24.thkit.ee" },
-  { eesnimi: "Marek", perekonnanimi: "Lukk", sugu: "Mees", link: "https://mareklukk24.thkit.ee" },
-  { eesnimi: "Nikita", perekonnanimi: "Nikiforov", sugu: "Mees", link: "https://nikitanikiforov24.thkit.ee" },
-  { eesnimi: "Nikita", perekonnanimi: "Orlenko", sugu: "Mees", link: "https://nikitaorlenko24.thkit.ee" },
-  { eesnimi: "Milan", perekonnanimi: "Petrovski", sugu: "Mees", link: "https://milanpetrovski24.thkit.ee" },
-  { eesnimi: "Adriana", perekonnanimi: "Pikaljov", sugu: "Naine", link: "https://adrianapikaljov24.thkit.ee" },
-  { eesnimi: "Mariia", perekonnanimi: "Posvystak", sugu: "Naine", link: "https://mariiaposvystak24.thkit.ee" },
-  { eesnimi: "Roman", perekonnanimi: "Prikaztsikov", sugu: "Mees", link: "https://romanprikaztsikov24.thkit.ee" },
-  { eesnimi: "Artjom", perekonnanimi: "Põldsaar", sugu: "Mees", link: "https://artjompoldsaar24.thkit.ee" },
-  { eesnimi: "Anastasiia", perekonnanimi: "Radasheva", sugu: "Naine", link: "https://anastasiiaradasheva24.thkit.ee" },
-  { eesnimi: "Oleksandra", perekonnanimi: "Ryshniak", sugu: "Naine", link: "https://oleksandraryshniak24.thkit.ee" },
-  { eesnimi: "Martin", perekonnanimi: "Rossakov", sugu: "Mees", link: "https://martinrossakov24.thkit.ee" },
-  { eesnimi: "Khussein", perekonnanimi: "Takhmazov", sugu: "Mees", link: "https://khusseintakhmazov24.thkit.ee" },
-  { eesnimi: "Maksim", perekonnanimi: "Tsikvasvili", sugu: "Mees", link: "https://maksimtsikvasvili24.thkit.ee" },
-  { eesnimi: "Roman", perekonnanimi: "Zaitsev", sugu: "Mees", link: "https://romanzaitsev24.thkit.ee" }
+  {
+    nimi: "Igor Aleksejev",
+    sugu: "mees",
+    link: "https://igoraleksejev24.thkit.ee",
+  },
+  {
+    nimi: "Illia Blahun",
+    sugu: "mees",
+    link: "https://illiablahun24.thkit.ee",
+  },
+  {
+    nimi: "Nikita Gontsarov",
+    sugu: "mees",
+    link: "https://nikitagontsarov24.thkit.ee",
+  },
+  {
+    nimi: "Mark Jürgen",
+    sugu: "mees",
+    link: "https://markjurgen24.thkit.ee",
+  },
+  {
+    nimi: "Nikita Litvinenko",
+    sugu: "mees",
+    link: "https://nikitalitvinenko24.thkit.ee",
+  },
+  {
+    nimi: "Marek Lukk",
+    sugu: "mees",
+    link: "https://mareklukk24.thkit.ee",
+  },
+  {
+    nimi: "Nikita Nikiforov",
+    sugu: "mees",
+    link: "https://nikitanikiforov24.thkit.ee",
+  },
+  {
+    nimi: "Nikita Orlenko",
+    sugu: "mees",
+    link: "https://nikitaorlenko24.thkit.ee",
+  },
+  {
+    nimi: "Milan Petrovski",
+    sugu: "mees",
+    link: "https://milanpetrovski24.thkit.ee",
+  },
+  {
+    nimi: "Adriana Pikaljov",
+    sugu: "naine",
+    link: "https://adrianapikaljov24.thkit.ee",
+  },
+  {
+    nimi: "Mariia Posvystak",
+    sugu: "naine",
+    link: "https://mariiaposvystak24.thkit.ee",
+  },
+  {
+    nimi: "Roman Prikaztsikov",
+    sugu: "mees",
+    link: "https://romanprikaztsikov24.thkit.ee",
+  },
+  {
+    nimi: "Artjom Põldsaar",
+    sugu: "mees",
+    link: "https://artjompoldsaar24.thkit.ee",
+  },
+  {
+    nimi: "Anastasiia Radasheva",
+    sugu: "naine",
+    link: "https://anastasiiaradasheva24.thkit.ee",
+  },
+  {
+    nimi: "Oleksandra Ryshniak",
+    sugu: "naine",
+    link: "https://oleksandraryshniak24.thkit.ee",
+  },
+  {
+    nimi: "Martin Rossakov",
+    sugu: "mees",
+    link: "https://martinrossakov24.thkit.ee",
+  },
+  {
+    nimi: "Khussein Takhmazov",
+    sugu: "mees",
+    link: "https://khusseintakhmazov24.thkit.ee",
+  },
+  {
+    nimi: "Maksim Tsikvasvili",
+    sugu: "mees",
+    link: "https://maksimtsikvasvili24.thkit.ee",
+  },
+  {
+    nimi: "Roman Zaitsev",
+    sugu: "mees",
+    link: "https://romanzaitsev24.thkit.ee",
+  },
 ];
 
 let kont = document.getElementById("opilased");
 let sisu = "";
 
-opilased.forEach((o, i) => {
+opilased.forEach((o) => {
   sisu += `
-    <div class="opilane" onclick="InfoF(${i})">
-      ${o.eesnimi} ${o.perekonnanimi}
-      <div id="info${i}" class="info" style="display:none;">
-        <p><strong>Eesnimi:</strong> ${o.eesnimi}</p>
-        <p><strong>Perekonnanimi:</strong> ${o.perekonnanimi}</p>
-        <p><strong>Sugu:</strong> ${o.sugu}</p>
-        <p><a href="${o.link}" target="_blank">Koduleht</a></p>
-      </div>
+    <div class="opilane ${o.sugu}" onclick="window.open('${o.link}', '_blank')">
+      ${o.nimi}
     </div>
   `;
 });
 
 kont.innerHTML = sisu;
-
-function InfoF(i) {
-  let info = document.getElementById("info" + i);
-  if (info.style.display == "none") {
-    info.style.display = "block";
-  }
-  else {
-    info.style.display = "none";
-  }
-}
