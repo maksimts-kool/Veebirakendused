19f69d7c6a6ed9c5f1a451b08e848c06ea614e78
maksimts-kool <maksimtsitkool@gmail.com>
2025-11-18 12:27:57 +0200

    Create veeb13

---
+++ html_tood/veeb13/content/ruhmaleht.php
@@ -0,0 +1,43 @@
+<body onload="algneSeadistus()">
+    <h1>Poodi veebikalkulaator</h1>
+    <table>
+        <tr>
+            <td>
+                <label><input type="radio" name="valikud" value="Images/1.png" onchange="muudaPiltiJaArvuta()"
+                        checked>Sai</label>
+                <label><input type="radio" name="valikud" value="Images/2.png"
+                        onchange="muudaPiltiJaArvuta()">Leib</label>
+                <label><input type="radio" name="valikud" value="Images/3.png"
+                        onchange="muudaPiltiJaArvuta()">Muffin</label>
+                <label><input type="radio" name="valikud" value="Images/4.png"
+                        onchange="muudaPiltiJaArvuta()">Tee</label>
+                <label><input type="radio" name="valikud" value="Images/5.png"
+                        onchange="muudaPiltiJaArvuta()">Kook</label>
+            </td>
+            <td>
+                <div id="valitudtekst">Valitud pilt:</div>
+            </td>
+            <td>
+                <img src="Images/1.png" alt="" id="pilt">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="kogus">Mitu tk soovid osta?</label>
+                <input type="number" id="kogus" name="kogus" min="1" max="100" value="1"
+                    onchange="arvutaKogusJaTarne()">
+            </td>
+            <td id="summatekst">Summa:</td>
+            <td id="summa"></td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <p>Vali tarneviis:</p>
+                <label><input type="radio" name="tarne" value="kohapeal" onchange="arvutaKogusJaTarne()"
+                        checked>Kaupluses (+0.00 EUR)</label>
+                <label><input type="radio" name="tarne" value="kuller" onchange="arvutaKogusJaTarne()">Kuller
+                    (+3.00 EUR)</label>
+            </td>
+        </tr>
+    </table>
+</body>
---
+++ html_tood/veeb13/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="?link=avaleht.php">Avaleht</a></li>
+        <li><a href="?link=phpkas.php">Git Käsud</a></li>
+        <li><a href="?link=ruhmaleht.php">Veebikalkulaator</a></li>
+        <li><a href="?link=ajafunk.php">Ajafunktsioonid</a></li>
+        <li><a href="http://maksimtsikvasvili24.thkit.ee/">Vana Index</a></li>
+    </ul>
+</nav>
