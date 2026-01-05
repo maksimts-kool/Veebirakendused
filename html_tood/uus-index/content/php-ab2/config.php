8cb93bb6f76cd4e89a981797397492d359525080
Maksim Tsikvasvili <maksimtsitkool@gmail.com>
2026-01-05 12:41:00 +0200

    Update config.php

--- html_tood/uus-index/content/php-ab2/config.php
+++ html_tood/uus-index/content/php-ab2/config.php
@@ -1,7 +1,7 @@
 <?php
 $serverinimi = "d141144.mysql.zonevs.eu";
 $kasutajanimi = "d141144_maksimts";
-$salasana = "Makism123.";
+$salasana = "";
 $andmebaasinnimi = "d141144_maksimts";
 $yhendus = new mysqli($serverinimi, $kasutajanimi, $salasana, $andmebaasinnimi);
 $yhendus->set_charset("utf8");
