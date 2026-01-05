75f4a85ea4c8a80946b5060d467fba99b1df29bb
Maksim Tsikvasvili <maksimtsitkool@gmail.com>
2026-01-05 12:58:08 +0200

    lisa1punkt uuendatud

--- html_tood/uus-index/content/valimised/funktsioonid.php
+++ html_tood/uus-index/content/valimised/funktsioonid.php
@@ -4,7 +4,6 @@ if (isset($_REQUEST["lisa1punkt"])) {
     $paring = $yhendus->prepare("UPDATE valimised SET punktid = punktid + 1 WHERE id=?");
     $paring->bind_param("i", $_REQUEST["lisa1punkt"]);
     $paring->execute();
-    header("Location: ".$_SERVER['PHP_SELF']);
     exit;
 }
 
